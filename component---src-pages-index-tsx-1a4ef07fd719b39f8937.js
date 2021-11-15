06d5d06d2504ddf8ece398b75231e6f669d45c96
Zasz123 <ygm12345@naver.com>
2021-11-15 23:16:20 +0900

    Updates

--- component---src-pages-index-tsx-1a4ef07fd719b39f8937.js
+++
@@ -1,2 +0,0 @@
-"use strict";(self.webpackChunkgatsby_starter_default=self.webpackChunkgatsby_starter_default||[]).push([[691],{6738:function(t,e,a){a.r(e);var u=a(7294),s=a(5444),f=a(5893);e.default=function(){return(0,u.useEffect)((function(){(0,s.navigate)("/article/1")}),[]),(0,f.jsx)("div",{})}}}]);
-//# sourceMappingURL=component---src-pages-index-tsx-1a4ef07fd719b39f8937.js.map
